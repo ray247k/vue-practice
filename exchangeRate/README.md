e85ba043c4a587f4d3b30db2b1725964c6b32a12
Qian <ray247k@gmail.com>
2019-01-01 18:59:20 +0800

    Add exchangeRate

---
+++ exchangeRate/README.md
@@ -0,0 +1,5 @@
+# exchangeRate
+exchange rate practice written in vue.js
+
+## TODO
+1. 接上 API 動態查詢
---
+++ exchangeRate/exchangeRate.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    var app = new Vue({
+        el:'#app',
+        data:{
+         NT:1000
+        },
+        computed:{
+          jpy: function(){
+            return this.NT/0.2713
+          },
+          usd: function(){
+            return this.NT/30.687
+          }
+        }
+      })
+}
